ed9d4bee6d35d37aaac8cdc487e261b17064ce28
chen.hengzhi@iwhalecloud.com <chen.hengzhi@iwhalecloud.com>
2020-09-07 10:24:31 +0800

    12

--- src/image.js
+++
@@ -1,9 +0,0 @@
-import img01 from './assets/img01.jfif';
-import img02 from './assets/img02.jfif';
-import img03 from './assets/img03.jfif';
-import img04 from './assets/img04.jfif';
-import img05 from './assets/img05.jfif';
-import img06 from './assets/img06.jfif';
-import img07 from './assets/img07.jfif';
-
-export default [img01, img02, img03, img04, img05, img06, img07];
